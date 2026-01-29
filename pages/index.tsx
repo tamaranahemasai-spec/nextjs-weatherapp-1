0eb77beffcf34e80b2e27526b52f7f24f71608b8
tamaranahemasai-spec <tamaranahemasai@gmail.com>
2026-01-29 10:50:36 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ interface WeatherData {
 }
 
 const Home = () => {
-  const [city, setCity] = useState('Dilshaj infotech'); // Default city
+  const [city, setCity] = useState('Dilshaj '); // Default city
   const [weather, setWeather] = useState<WeatherData | null>(null);
   const [error, setError] = useState<string | null>(null);
 
